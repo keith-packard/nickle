3fd7c058e9fdb239e49259513ccc49ef530fba32
Bart Massey <bart@cs.pdx.edu>
2001-04-24 18:23:28 +0000

    Updated TODO list off my whiteboard. Fixed missing bits in runbench.sh.

--- bench/runbench.sh
+++ bench/runbench.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 while read bmname
 do
-  echo "nickle $bmname.5c"
+  gcc -O4 -o "$bmname" "$bmname.c" -lgmp > /dev/null 2>&1 || exit 1
+  echo "../nickle $bmname.5c"
   echo "bc $bmname.bc"
   echo "./$bmname /dev/null"
 done <<'EOF' |

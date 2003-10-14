4076cffdcddad30ce9749faed34b97952bafb2a2
Bart Massey <bart@cs.pdx.edu>
2003-10-14 01:20:29 +0000

    Experimental changes to auto-increment the version number. I can't stand it any longer :-).

---
+++ update-version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+FILE=version.m4
+CURHEADER="`sed -e '/CURHEADER/!d' -e 's=^.*\$\(.*\)\$$=\1=' <$FILE`"
+OLDHEADER="`sed -e '/OLDHEADER/!d' -e 's=^.*<\(.*\)>$=\1=' <$FILE`"
+CURVERSION="`sed -e '/CURVERSION/!d' -e 's=^[^0-9]*\(.*\)$=\1=' <$FILE`"
+
+if [ "$CURHEADER" != "$OLDHEADER" ]
+then
+    CURVERSION="`echo \"$CURVERSION\" |
+                 awk -F. '{printf("%d.%d\n", $1, $2+1);}'`"
+    OLDHEADER="$CURHEADER"
+fi
+
+echo dnl 'This file is maintained automatically by update-version.sh'
+echo dnl 'The file format is finicky, so do not hand-edit lightly.'
+echo dnl 'Last updated' "`date`"
+echo dnl CURHEADER "\$$CURHEADER\$"
+echo dnl OLDHEADER "<$OLDHEADER>"
+echo dnl CURVERSION $CURVERSION
+echo "define([VERSION_NUMBER],[$CURVERSION])"
---
+++ version.m4
@@ -0,0 +1,4 @@
+dnl CURHEADER $Header$
+dnl OLDHEADER <Header: /local/src/CVS/nickle/version.m4,v 1.00 2003/10/13 00:01:00 bart Exp >
+dnl CURVERSION 2.20
+define([VERSION_NUMBER],[2.20])

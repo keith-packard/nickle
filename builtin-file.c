ee82a7b31b94ee2cb95fb1b9cda315d5eb69705c
Keith Packard <keithp@keithp.com>
2004-08-23 00:42:06 +0000

    Base is allowed to be zero (in which case it uses 10).

--- builtin-file.c
+++ builtin-file.c
@@ -215,10 +215,10 @@ do_File_print (Value file, Value value, Value format,
     ibase = IntPart (base, "Illegal base");
     if (aborting)
 	return Void;
-    if (ibase < 2) {
+    if (ibase < 0 || ibase == 1)
+    {
 	RaiseStandardException (exception_invalid_argument,
-				"base is less than 2",
-				2, NewInt (0), base);
+				"Illegal base", 2, NewInt (0), base);
 	return Void;
     }
     iwidth = IntPart (width, "Illegal width");

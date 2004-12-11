c0400042d277b792b0c93924027e2229bc27ba52
Keith Packard <keithp@keithp.com>
2004-12-11 17:42:18 +0000

    Forgot to add actual foreign dataype and interface code.

---
+++ builtin-foreign.c
@@ -0,0 +1,81 @@
+/* $Header$ */
+
+/*
+ * Copyright © 2004 Keith Packard and Bart Massey.
+ * All Rights Reserved.  See the file COPYING in this directory
+ * for licensing information.
+ */
+
+#include	<ctype.h>
+#include	<strings.h>
+#include	<time.h>
+#include	"builtin.h"
+
+NamespacePtr	ForeignNamespace;
+
+#if HAVE_EXTERN_SYMS
+
+#if HAVE_DLFCN_H && HAVE_LIBDL
+#define HAVE_FOREIGN_LOAD 1
+#include	<dlfcn.h>
+
+static Value
+do_Foreign_load (Value av)
+{
+    ENTER ();
+    char    *name = StrzPart (av, "invalid foreign module name");
+    void    *lib;
+    Value   (*init) (void);
+    Value   ret = False;
+
+    if (!name)
+	RETURN (Void);
+    
+    lib = dlopen (name, RTLD_NOW);
+    if (!lib)
+    {
+	char	*err = dlerror ();
+	if (!err)
+	    err = "cannot open";
+	RaiseStandardException (exception_invalid_argument,
+				err, 2, NewInt(0), av);
+	RETURN (Void);
+    }
+    
+    init = (Value (*) (void)) dlsym (lib, "nickle_init");
+    if (!init)
+    {
+	char	*err = dlerror ();
+	if (!err)
+	    err = "missing nickle_init";
+	RaiseStandardException (exception_invalid_argument,
+				err, 2, NewInt (0), av);
+	dlclose (lib);
+	RETURN (Void);
+    }
+    ret = (*init) ();
+    RETURN (ret);
+}
+#endif
+
+#endif
+
+void
+import_Foreign_namespace()
+{
+    ENTER ();
+    static const struct fbuiltin_1 funcs_1[] = {
+#if HAVE_EXTERN_SYMS && HAVE_FOREIGN_LOAD
+	{ do_Foreign_load, "load", "b", "s", "\n"
+	    " bool load (string name)\n"
+	    "\n"
+	    " Load a foreign library into nickle\n" },
+#endif
+	{ 0 }
+    };
+
+    ForeignNamespace = BuiltinNamespace (/*parent*/ 0, "Foreign")->namespace.namespace;
+
+    BuiltinFuncs1 (&ForeignNamespace, funcs_1);
+    EXIT ();
+}
---
+++ foreign.c
@@ -0,0 +1,90 @@
+/* $Header$ */
+
+/*
+ * Copyright © 2004 Keith Packard and Bart Massey.
+ * All Rights Reserved.  See the file COPYING in this directory
+ * for licensing information.
+ */
+
+#include	"nickle.h"
+
+static Value
+ForeignEqual (Value av, Value bv, int expandOk)
+{
+    if (av == bv)
+	return TrueVal;
+    return FalseVal;
+}
+
+static Bool
+ForeignPrint (Value f, Value av, char format, int base, int width, int prec, int fill)
+{
+    FilePrintf (f, "foreign %s (0x%x)", av->foreign.id, av->foreign.data);
+    return True;
+}
+
+static HashValue
+ForeignHash (Value av)
+{
+    return (HashValue) av->foreign.data;
+}
+
+static void
+ForeignMark (void *object)
+{
+}
+
+static int
+ForeignFree (void *object)
+{
+    Foreign *foreign = object;
+
+    if (foreign->free)
+	(*foreign->free) (foreign->data);
+    return 0;
+}
+
+ValueRep ForeignRep = {
+    { ForeignMark, ForeignFree, "ForeignRep" },	    /* data */
+    rep_foreign,	    /* tag */
+    {		    /* binary */
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	ForeignEqual,
+	0,
+	0
+    },
+    {
+	0,
+	0,
+	0,
+    },
+    0, 0,
+    ForeignPrint,
+    0,
+    ForeignHash,
+};
+
+int
+ForeignInit (void)
+{
+    return 1;
+}
+
+Value
+NewForeign (const char *id, void *data, void (*free) (void *data))
+{
+    ENTER ();
+    Value   ret;
+
+    ret = ALLOCATE (&ForeignRep.data, sizeof (Foreign));
+    ret->foreign.id = id;
+    ret->foreign.data = data;
+    ret->foreign.free = free;
+    RETURN (ret);
+}

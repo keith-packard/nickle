816ee6f7a8011e4bd79e529a431ab1c457ff9906
Keith Packard <keithp@keithp.com>
2024-11-27 15:34:47 -0800

    autogen: Support building in subdir
    Use the path to autogen.sh to find the top source dir
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@
 # runs autotools to create ./configure and friends
 #
 # configure depends on version.m4, but autoreconf does not realize this
-rm configure
-autoreconf -Wall -v --install || exit 1
-./configure "$@"
+dir=`dirname $0`
+rm $dir/configure
+(cd $dir && autoreconf -Wall -v --install) || exit 1
+$dir/configure "$@"
